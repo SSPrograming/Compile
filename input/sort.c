2b7b3d393290c52406841346d64a375adc697a92
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-12-08 21:41:28 +0800

    change sort.c

--- input/sort.c
+++ input/sort.c
@@ -1,32 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void sort(int* arr, int n) {
-	int i, j, change = 0;
-	for (i = n - 1; i >= 0; i--) {
-		for (j = 0; j < i; j++) {
-			if (arr[j] > arr[j + 1]) {
-				arr[j] = arr[j + 1] + arr[j];
-				arr[j + 1] = arr[j] - arr[j + 1];
-				arr[j] = arr[j] - arr[j + 1];
-				change = 1;
-			}
-		}
-		if (change == 0) break;
-	}
+void sort(int* arr, int n) 
+{
+    int i = n - 1;
+    int j = 0;
+    int change = 0;
+    while (i >= 0) {
+        j = 0;
+        while (j < i) {
+            if (arr[j] > arr[j + 1]) {
+                arr[j] = arr[j + 1] + arr[j];
+                arr[j + 1] = arr[j] - arr[j + 1];
+                arr[j] = arr[j] - arr[j + 1];
+                change = 1;
+            }
+            j = j + 1;
+        }
+        if (change == 0) { break; }
+        i = i - 1;
+    }
 }
 
-int main() {
-	int i, n;
-	scanf("%d", &n);
-	int* arr = malloc(sizeof(int) * n);
-	for (i = 0; i < n; i++) {
-		scanf("%d", arr + i);
-	}
-	sort(arr, n);
-	for (i = 0; i < n; i++) {
-		printf("%d ", arr[i]);
-	}
-	printf("\n");
-	return 0;
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int* arr = malloc(sizeof(int) * n);
+    int i = 0;
+    while (i < n) {
+        scanf("%d", arr + i);
+        i = i + 1;
+    }
+    sort(arr, n);
+    i = 0;
+    while (i < n) {
+        printf("%d ", arr[i]);
+        i = i + 1;
+    }
+    printf("\n");
+    return 0;
 }
