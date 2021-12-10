630905b1d5b7829ffa9da0bde6003a5ee5a1b905
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2021-12-10 20:00:13 +0800

    准备求和

---
+++ input/sum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum(int* a, int n){
+    int _sum = 0;
+    n = n - 1;
+    while (n >= 0) {
+        _sum = _sum + a[n];
+        n = n - 1;
+    }
+    return _sum;
+}
+
+int main() {
+    int a[3];
+    a[0] = 0;
+    a[1] = 1;
+    a[2] = 2;
+    printf("sum(0, 1, 2) = %d\n", sum(a, 3));
+    return 0;
+}
